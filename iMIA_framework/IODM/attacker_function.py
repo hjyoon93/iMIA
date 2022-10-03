9a67657dcff6d129b398659b210166813b749237
AshrithReddyT <ashrithreddyt@gmail.com>
2022-10-04 01:47:32 +0530

    Few fixes

--- iMIA_framework/IODM/attacker_function.py
+++ iMIA_framework/IODM/attacker_function.py
@@ -3,20 +3,18 @@ import numpy as np
 
 def DoS(G_real, G_att, G_def, monit_time):
     node_id_set = list(G_att.nodes())
-    if random.random() <= G_real.nodes[random_id]["vulnerability"] * math.exp(-1/monit_time): 
+    if random.random() <= G_real.nodes[random_id]["vulnerability"]: 
         G_real.nodes[random_id]["compromised"] = True
         G_att.nodes[random_id]["compromised"] = True
 
 def dataManipulation(G_real, G_att, G_def, monit_time):
     node_id_set = list(G_att.nodes())
-    if random.random() >= G_real.nodes[random_id]["integrity"] * math.exp(-monit_time): 
-        G_real.nodes[random_id]["integrity"] = G_real.nodes[random_id]["integrity"] - 0.1
+    if random.random() <= G_real.nodes[random_id]["vulnerability"]: 
         G_real.nodes[random_id]["compromised"] = True
         G_att.nodes[random_id]["compromised"] = True
 
 def Phishing(G_real, G_att, G_def, monit_time):
     node_id_set = list(G_att.nodes())
-    if random.random() >= G_real.nodes[random_id]["confidentiality"] * math.exp(-monit_time): 
-        G_real.nodes[random_id]["confidentiality"] = G_real.nodes[random_id]["confidentiality"] - 0.2
+    if random.random() <= G_real.nodes[random_id]["vulnerability"]:
         G_real.nodes[random_id]["compromised"] = True
         G_att.nodes[random_id]["compromised"] = True
--- iMIA_framework/IODM/simulation.py
+++ iMIA_framework/IODM/simulation.py
@@ -4,6 +4,8 @@ import numpy as np
 import json 
 
 from graph_function import *
+from attacker_function import *
+from defender_function import *
 
 def save_graph(G):
     all_nodes = list(G.nodes())
@@ -20,10 +22,62 @@ if __name__ == '__main__':
     set_compromised(G)
     set_centrality(G)
     set_vulnerability(G)
-    
+    attacker_subgame = 0
+    sub_game_number = 3
     # while steps > 0:
 
     save_graph(G)
+
+    game_continue = True
+    while (game_continue):
+        system_lifetime += 1
+
+        print("Game: " + str(system_lifetime))
+
+        print("Stage: " + str(attacker_subgame + 1))
+        
+
+        def_subgame = def_predict_stage(defender_uncertainty_update()[0], attacker_subgame)
+        
+        if def_subgame == 3:
+            def_chosen_strategy = random.randint(0,4)
+            print("defender select full game")
+        else:
+            # defender select strategy
+            def_chosen_strategy = defender_choose_strategy(def_subgame)
+            print("defender strategy: "+str(def_chosen_strategy + 1))
+
+        
+        # defender behavior
+        defender_behavior(def_chosen_strategy)
+        
+        # attacker select strategy
+        att_chosen_strategy = attacker_choose_strategy(att_subgame)
+        print("attacker strategy: " + str(att_chosen_strategy + 1))
+
+        # attacker behavior
+        action_result = attacker_behavior(att_chosen_strategy)
+        
+        
+        # changing stage
+        if action_result:
+            if att_chosen_strategy == 0 and att_subgame == 1:
+                print("remain in Stage D")
+            elif att_subgame >= sub_game_number-1:
+                print("stay in stage 3")
+            else:
+                print("move to next stage")
+                attacker_subgame += 1
+            # new game, new impact
+            attack_impact_record = np.zeros(att_strategy_number)
+            defend_impact_record = np.zeros(def_strategy_number)
+        
+
+        attacker_action_history[attacker_subgame, att_chosen_strategy] += 1
+        update_strategy_probability()
+        
+        if is_system_fail():
+            game_continue = False
     # print(G.edges())
     # print(G)
         # print(nx.cycle_basis(G.to_undirected()))
